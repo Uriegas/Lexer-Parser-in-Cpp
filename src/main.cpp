d5dc1ff2b619dc52d9add39fcf92c79f8f699289
Uriegas <1930526@upv.edu.mx>
2020-07-29 12:36:02 -0500

    Cleaned Code

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,26 @@
 //Lexer_Token Parser -> String Formula Analyzer
 
 //The code is divided into:
-//Enums
+//Enums and Struct
 //Support functions
 //lexer_part_1 function     tokenizer
 //lexer_part_2 function     error handling
 //Main function
 
 //Just for using names instead of numbers in the code to tokenize the string
-enum STRING{ NUMBER, VARIABLE, FUNCTION, OPERATOR, OPEN_FUNC, CLOSE_FUNC, OPEN_VAR, CLOSE_VAR, OPEN_PAR, CLOSE_PAR, SEPARATOR };
+enum STRING{    NUMBER, VARIABLE, FUNCTION, OPERATOR, OPEN_FUNC, CLOSE_FUNC, 
+                OPEN_VAR, CLOSE_VAR, OPEN_PAR, CLOSE_PAR, SEPARATOR             };
 
-enum ERRORS{ NOERROR = 1, SYNTAXERROR, MISSING_VARIABLE, MISSING_BRACKET, MISSING_PARENTHESIS, MISSING_COMMA, MISSING_FUNCTION };
+enum ERRORS{    NOERROR = 1, SYNTAXERROR, MISSING_VARIABLE, MISSING_BRACKET, 
+                MISSING_PARENTHESIS, MISSING_COMMA, MISSING_FUNCTION            };
 
 //Simple Data Structure for tokenization
 struct tokens{
     int ID;
     std::string value;
 };
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<LEXER PART 1 FUNCTIONS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 //All of this are bool functions implemented in the lexical analyzer function
 bool operator==(const tokens& compare1, const tokens compare2){
@@ -90,8 +94,40 @@ tokens select_special_character(char a){
     return res;
 }
 
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<LEXER PART 2 FUNCTIONS>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//Prints the kind of error
+void return_error(int err){
+    switch (err){
+    case SYNTAXERROR:
+        std::cout << "Syntax Error, maybe a missing operand";
+        break;
+    case MISSING_VARIABLE:
+        std::cout << "Error in variable name, you should enter just letters and numbers in it";
+        break;
+    case MISSING_BRACKET:
+        std::cout << "Missing bracket: '{' or '}'";
+        break;
+    case MISSING_PARENTHESIS:
+        std::cout << "Missing parenthesis '(' or ')'";
+        break;
+    case MISSING_COMMA:
+        std::cout << "Error in function parameters, maybe there is a missing comma or closing parenthesis";
+        break;
+    case MISSING_FUNCTION:
+        std::cout << "Misspelling in function prototype, this function does not exist";
+        break;
+    default:
+        std::cout << "There is no error";
+        break;
+    }
+    std::cout << "\n";
+}
+
 //To future implementation
 std::string evaluate_negative_sign(){}
+
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<LEXER PART 1>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 //Lexical analyzer, lexer or tokenizer function
 //It scans a string and divides it into tokens
@@ -205,33 +241,8 @@ std::vector <tokens> lexer_part_1(std::string string){
     return tokenized_string;
 }
 
-//Prints the kind of error
-void return_error(int err){
-    switch (err){
-    case SYNTAXERROR:
-        std::cout << "Syntax Error, maybe a missing operand";
-        break;
-    case MISSING_VARIABLE:
-        std::cout << "Error in variable name, you should enter just letters and numbers in it";
-        break;
-    case MISSING_BRACKET:
-        std::cout << "Missing bracket: '{' or '}'";
-        break;
-    case MISSING_PARENTHESIS:
-        std::cout << "Missing parenthesis '(' or ')'";
-        break;
-    case MISSING_COMMA:
-        std::cout << "Error in function parameters, maybe there is a missing comma or closing parenthesis";
-        break;
-    case MISSING_FUNCTION:
-        std::cout << "Misspelling in function prototype, this function does not exist";
-        break;
-    default:
-        std::cout << "There is no error";
-        break;
-    }
-    std::cout << "\n";
-}
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<LEXER PART 2>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 //Second iteration over string
 //Returns 1 if there is not an error
 int lexer_part_2(std::vector <tokens>& string){
@@ -280,6 +291,8 @@ int lexer_part_2(std::vector <tokens>& string){
         return NOERROR;//We ended the looping without errors
 }
 
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<LEXER PART 3>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 //Third interation over string, now to convert functions to operators
 //Also evaluates the correctness of the syntax
 std::vector<tokens> lexer_part_3(std::vector <tokens> string){
@@ -293,182 +306,25 @@ std::vector<tokens> lexer_part_3(std::vector <tokens> string){
 }
 //std::stack <tokens> parser(std::vector <tokens> string){}
 
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<MAIN FUNCTION>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 int main(){
     int lex2; //Variable to save the error result of lex2
     std::string string = "pow(({yosoy}+5),2)/(sin(4.5)*{elpofe123})";
     std::vector<tokens> tokenized_string = lexer_part_1(string);
     std::cout << "First String Tokenization\t " << string << "\t tokenized to: \n";
 
+    //Print vector
     for(int i = 0; i < tokenized_string.size(); i++)
         std::cout << tokenized_string[i].ID << "\t" << tokenized_string[i].value << "\n";
         
     lex2 = lexer_part_2(tokenized_string);
 
+    //If there is no error the function returnerd 1
     if(lex2 == 1){
         std::cout << "Second String Iteration\t " << string << "\t tokenized to: \n";
         for(int i = 0; i < tokenized_string.size(); i++)
             std::cout << tokenized_string[i].ID << "\t" << tokenized_string[i].value << "\n";
     }
-    else
+    else//If there is error evaluate wich one
         return_error(lex2);
 }
-
-/*
-// function to split string into substrings on the 
-// basis of delimiter and return the substrings 
-// after split in a vector of strings
-std::vector<std::string> split_string(std::string str, char dl){
-    std::string word = ""; 
-    // to count the number of split strings 
-    int num = 0; 
-    // adding delimiter character at the end 
-    // of 'str' 
-    str = str + dl; 
-    // length of 'str' 
-    int l = str.size(); 
-    // traversing 'str' from left to right 
-    std::vector<std::string> substr_list; 
-    for (int i = 0; i < l; i++) { 
-        // if str[i] is not equal to the delimiter 
-        // character then accumulate it to 'word' 
-        if (str[i] != dl) 
-            word = word + str[i]; 
-        else { 
-            // if 'word' is not an empty string, 
-            // then add this 'word' to the array 
-            // 'substr_list[]' 
-            if ((int)word.size() != 0) 
-                substr_list.push_back(word); 
-            // reset 'word' 
-            word = ""; 
-        } 
-    } 
-    // return the splitted strings 
-    return substr_list; 
-}
-
-//This are the steps for creating the algorithm
-//First, split the string into tokens (tokenization)
-//Tokens can be numbers, operands, and parenthesis
-//The algorithm reads tokens and uses the following logic
-//If it is a number token, it is pushed on a number stack (or the output queue, if we were generating RPN)
-//If it is an operator, pop operators from the operator stack and evaluate them with values from the number stack, until we reach one that has lower precedence. Then we push the original operator.
-//In the case of a right associative operator, we also stop if we reach an operator of the same precedence.
-//If it a left parenthesis, just push it.
-//If it is a right parenthesis, we pop all operators (and evaluate them) until the matching left parenthesis. The parentheses are thrown away.
-//After all tokens are read, the remaining operators on stack are popped and evaluated.
-
-//https://en.wikipedia.org/wiki/Shunting-yard_algorithm
-// with small adjustment for expressions in functions. Wiki example works only for constants as arguments
-//Implementation on Java can be found at:
-//https://github.com/vtsyvina/sim-it/blob/master/src/main/java/sim/grammar/SimItGrammarParser.java#L248-L326
-
-//std::stack<std::string> stack = new stack<>();
-//std::initializer_list<Token> result = new ArrayList<>();
-
-//Tokenization Section
-//Tokenizer Code
-//Could create a data strucutre that tokenize the variables in the queue
-//Ej. {x}, variable
-//      +, operand
-//      pow, function
-std::queue <std::string> tokenizer(std::string& formula){
-    std::queue<std::string> tokenized_formula;
-    std::string buffer;
-
-    for( int i = 0; i < formula.size(); i++){
-        buffer += formula[i];
-        if(formula[i-1] != operand()){
-            if(formula[i] == '('){ //It is an openining parenthesis
-                //It is a function
-            }
-            else{//It is a conjunction parenthesis
-                tokenized_formula.push(buffer);
-            }
-        }
-        else if (formula[i] == ')'){
-            tokenized_formula.push(buffer);
-        }
-        else if (formula == '{'){ //Begins a variable
-            buffer += formula[i];
-            continue;
-        }
-        else if(formula == '}'){ //Its the end of a variable
-            buffer += formula;
-            tokenized_formula.push(buffer);
-        }
-        buffer.clear();
-    }
-}
-for (Token token : tokens) {
-    if (isNumber(token) || isIdentifier(token)) {
-        result.add(token);
-        continue;
-    }
-    if (isFunction(token)) {
-        stack.push(token);
-        continue;
-    }
-
-
-    // if OP(open parentheses) then put to stack
-    if (isOP(token)) {
-        stack.push(token);
-        continue;
-    }
-    // CP(close parentheses) pop stack to result until OP
-    if (isCP(token)) {
-        Token cur = stack.pop();
-        while (!isOP(cur)) {
-            if (!isComma(cur)) {
-                result.add(cur);
-            }
-            cur = stack.pop();
-        }
-        continue;
-    }
-    if (isBinaryOperation(token)) {
-        if (!stack.empty()) {
-            Token cur = stack.peek();
-            while ((!isBinaryOperation(cur)
-                    || (isBinaryOperation(cur) && hasHigherPriority(cur, token))
-                    || (hasEqualPriority(cur, token) && isLeftAssociative(token)))
-                    && !isOP(cur)
-            ) {
-                // no need in commas in resulting list if we now how many parameters the function need
-                if (!isComma(cur)) {
-                    result.add(cur);
-                }
-
-                stack.pop();
-                if (!stack.empty()) {
-                    cur = stack.peek();
-                }
-            }
-        }
-        stack.push(token);
-        continue;
-    }
-
-    if (isComma(token)) {
-        Token cur = stack.peek();
-        while (!(isOP(cur) || isComma(cur))) {
-            result.add(cur);
-            stack.pop();
-            if (!stack.empty()) {
-                cur = stack.peek();//  don't pop if priority is less
-            }
-        }
-        stack.push(token);
-
-    }
-}
-while (!stack.empty()) {
-    Token pop = stack.pop();
-    if (!isComma(pop)) {
-        result.add(pop);
-    }
-
-}
-return result;
-*/
